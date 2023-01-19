b98b68dc7212e384662f9797314a17633d843d87
Rakesh Doppalapudi <rakesh.doppalapudi@yash.com>
2023-01-19 15:44:34 +0530

    Add project files.

---
+++ AutomationCRM/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationCRM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            XrmApp xrm = new XrmApp();
+
+        }
+    }
+}
