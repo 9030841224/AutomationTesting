c64d4eff5e7dcb4ff15f7aff759bc35fdcaf63b4
Rakesh Doppalapudi <rakesh.doppalapudi@yash.com>
2023-01-19 15:58:20 +0530

    Test Merge

--- Microsoft.Dynamics365.UIAutomation.Sample/Sprints/Sprint1/Sprint1.cs
+++ Microsoft.Dynamics365.UIAutomation.Sample/Sprints/Sprint1/Sprint1.cs
@@ -23,9 +23,7 @@ namespace Microsoft.Dynamics365.UIAutomation.Sample.Sprints.Sprint1
             using (var xrmApp = new XrmApp(client))
             {
                 xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
-
                 xrmApp.Navigation.OpenApp(UCIAppName.Sales);
-
                 xrmApp.Navigation.OpenSubArea("Sales", "Accounts");
                 xrmApp.CommandBar.ClickCommand("New");
                 String BusinessName = "Test Account";
